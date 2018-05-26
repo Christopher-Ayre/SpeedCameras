caf096220627570f4e4e0cd44a0fa179c6ac03d4
Christopher Ayre <christopher.develop@hotmail.com>
2018-05-26 23:05:28 +0800

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 echo "You will need to make sure TKinter is installed manually"
 
-pip3 install --user tabula-py
-pip3 install --user pyPdf
-pip3 install --user bs4
-pip3 install --user requests
-
 pip install --user tabula-py
 pip install --user pyPdf
 pip install --user bs4
