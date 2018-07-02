fcafa6ff5b7a56249e02b3b1c111a7c45dea505e
Christopher Ayre <christopher.develop@hotmail.com>
2018-07-02 20:31:21 +0800

    Finished SpeedCameraFactory

--- SpeedCameraFactory.py
+++ SpeedCameraFactory.py
@@ -1,16 +1,34 @@
+from RedLightCamera import RedLightCamera
+from FixedCamera import FixedCamera
+from VariableCamera import VariableCamera
+
 class SpeedCameraFactory:
 
+    
 
-    def makeCamera( self, string ):
+    def makeCamera( self, suburb, location ):
 
         #If the string contains " and " or " & " then it is a Red Light Camera
         #If the string ends with " #F" then it is a Fixed Camera
         #Otherwise it is a variable camera
 
-        if " and " in string or " & " in string:
-            print( "RedLightCamera")
-        elif " #F" in string:
-            print( "FixedCamera" )
+        if " and " in location or " & " in location:
+
+            #Couldn't get a regex to split at both
+            streets = location.split( " and " )
+
+            if len( streets ) < 2:
+                streets = location.split( " & " )
+
+            return RedLightCamera( suburb, streets[0], streets[1] )
+
+        elif " #F" in location:
+
+            #remove added ending
+            location = location[:-3]
+
+            return FixedCamera( suburb, location )
         else:
-            print( "Variable Camera" )
+            
+            return VariableCamera( suburb, location )
 
