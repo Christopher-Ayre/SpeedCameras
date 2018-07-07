7ea6b288040c5cee8403d66229a8f3718741a170
Christopher Ayre <christopher.develop@hotmail.com>
2018-07-07 15:47:15 +0800

    Finished pdf downloader

--- PdfDownloader.py
+++ PdfDownloader.py
@@ -3,16 +3,19 @@
 # Change all output to logging
 
 
+import urllib.request
+import requests
+import re
 import time
 import datetime
 import os
 from bs4 import BeautifulSoup
-from pyPdf import PdfFileReader
+from PyPDF2 import PdfFileReader
 from tabula import read_pdf
 
 class PdfDownloader:
 
-    def __init__( self ):
+    def __init__( self, ui ):
 
         self.url = "http://www.police.wa.gov.au/Traffic/Cameras/Camera-locations"
         self.fileName = "locations.pdf"
@@ -25,19 +28,23 @@ class PdfDownloader:
 
         # The same format the dates are in the PDF
         self.currentDate = weekDay.upper() + " " + date + " " + monthName.upper() + " " + year
+        
+        #Used for logging
+        self.ui = ui
 
-    def returnLoacations(self):
+    def returnLocations(self):
 
-        downloadPDF( findCurrentPDF() )
+        self.downloadPDF( self.findCurrentPDF() )
 
+        self.locations = self.findAllLocations()
 
-        locations = findReleventLocations( myLocations )
+        self.cleanUp( self.fileName, self.locations )
 
-        displayLocations( locations )
-
-        cleanUp()
+        return self.locations
 
     def findCurrentPDF(self):
+
+        self.ui.log( self, "Finding PDF for current time period" )
 
         # Find today
         today = datetime.date.today()
@@ -47,7 +54,7 @@ class PdfDownloader:
 
         startOfWeekString = startOfWeek.strftime('%d%m%Y')
 
-        sourceCode = requests.get( url )
+        sourceCode = requests.get( self.url )
         sourceCode = sourceCode.text
         htmlCode = BeautifulSoup( sourceCode, 'html.parser' )
 
@@ -62,37 +69,77 @@ class PdfDownloader:
         # Concatenate strings to get final link url
         pdfLink = "http://www.police.wa.gov.au" + firstLink
 
+        self.ui.log( self, "PDF Found!" )
+
         return pdfLink
 
 
     def downloadPDF( self, url ):
 
-        print "Downloading PDF"
-        urllib.urlretrieve( url, fileName )
-        print "Download Complete!\n"
+        self.ui.log( self, "Downloading PDF" )
+
+        urllib.request.urlretrieve( url, self.fileName )
+
+        self.ui.log( self, "Download Complete!" )
 
 
 
     # Parse PDF
-    def findReleventTable(self):
+    def findAllLocations(self):
         # Consider what to do near midnight
 
-        print "Finding all today's cameras\n"
-        table = read_pdf( fileName, multiple_tables = True, pages = "all" )
+        self.ui.log( self, "Finding all today's cameras" )
 
-        locations = "Error!"
+        table = read_pdf( self.fileName, multiple_tables = True, pages = "all" )
+
+        locationTable = "Error!"
 
         # Loop through table and find the one corresponding to todays date
         for ii in table:
-            if ii[0][0] == currentDate:
-                locations = ii
+            if ii[0][0] == self.currentDate:
+                locationTable = ii
 
-        # Newlines to remove errors from view
-        print "\n" * 50
-        print "All cameras found!\n"
+        self.ui.log( self, "All cameras found!" )
 
+        self.ui.log( self, "Parsing tables" )
+
+        suburbsOne = locationTable[1]
+        suburbsTwo = locationTable[3]
+        streetsOne = locationTable[0]
+        streetsTwo = locationTable[2]
+
+        print( suburbsOne + "\n\n" )
+        print( suburbsTwo + "\n\n" )
+        print( streetsOne + "\n\n" )
+        print( streetsTwo + "\n\n" )
+
+        # Empty tuple to hold Suburb and street names
+        locations = []
+
+        # Iterate through first list
+        # Try to join lists again later
+        pos = 0
+        for ii in suburbsOne:
+            locations.append((ii,streetsOne[pos]))
+            pos = pos + 1
+
+        # Iterate through second list
+        pos = 0
+        for ii in suburbsTwo:
+            locations.append((ii,streetsTwo[pos]))
+            pos = pos + 1
+
+        print( locations )
         return locations
 
     # Delete any temp files
-    def cleanUp(self):
+    def cleanUp(self, fileName, locations ):
+
+        self.ui.log( self, "Removed file: " + fileName )
+
+        for ii in locations:
+            if (ii[0] != ii[0]) or ( ii == ('Suburb', 'Street Name') ):
+                print( ii[0])
+                locations.remove(ii)
+
         os.remove( fileName )
