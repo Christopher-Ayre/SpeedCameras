e51dc93e6d2933f8ad8c08271a5f261449122083
Christopher Ayre <christopher.develop@hotmail.com>
2018-07-09 16:32:42 +0800

    CameraController added to start stiching classes together

---
+++ CameraController.py
@@ -0,0 +1,24 @@
+class CameraController:
+
+    def __init__( self, ui, camFetcher, factory ):
+        self.ui = ui
+        self.camFetcher = camFetcher
+        self.factory = factory
+        self.locations = None
+        self.cameras = []
+
+    #Load or reload camera locations
+    def loadCamLocations( self ):
+
+        #TODO: also get cameras that arn't stored in pdf. i.e. Fixed and RedLight 
+        self.locations = self.camFetcher.getLocations()
+
+
+    def buildCameras( self ):
+        if self.locations:
+            for cam in self.locations:
+                cam = self.factory.makeCamera( cam[0], cam[1] )
+
+                #Not null
+                if cam:
+                    self.cameras.append( cam )
